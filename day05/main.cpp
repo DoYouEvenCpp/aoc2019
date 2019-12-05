93de4ecee2f3d127b0a99fa5fbf7d2853e0a4f94
Stanislaw Kubiak <extern.stanislaw.kubiak@elektrobit.com>
2019-12-05 15:46:54 +0100

    Rework needed for day 5 (not working, btw xD)

--- day05/main.cpp
+++ day05/main.cpp
@@ -44,7 +44,9 @@ struct OpCode {
 };
 
 auto SECOND_PUZZLE = 19690720;
-std::vector input_org = { 1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,10,19,1,6,19,23,1,10,23,27,2,27,13,31,1,31,6,35,2,6,35,39,1,39,5,43,1,6,43,47,2,6,47,51,1,51,5,55,2,55,9,59,1,6,59,63,1,9,63,67,1,67,10,71,2,9,71,75,1,6,75,79,1,5,79,83,2,83,10,87,1,87,5,91,1,91,9,95,1,6,95,99,2,99,10,103,1,103,5,107,2,107,6,111,1,111,5,115,1,9,115,119,2,119,10,123,1,6,123,127,2,13,127,131,1,131,6,135,1,135,10,139,1,13,139,143,1,143,13,147,1,5,147,151,1,151,2,155,1,155,5,0,99,2,0,14,0 };
+std::vector input_org = {3,225,1,225,6,6,1100,1,238,225,104,0,1102,59,58,224,1001,224,-3422,224,4,224,102,8,223,223,101,3,224,224,1,224,223,223,1101,59,30,225,1101,53,84,224,101,-137,224,224,4,224,1002,223,8,223,101,3,224,224,1,223,224,223,1102,42,83,225,2,140,88,224,1001,224,-4891,224,4,224,1002,223,8,223,1001,224,5,224,1,223,224,223,1101,61,67,225,101,46,62,224,1001,224,-129,224,4,224,1002,223,8,223,101,5,224,224,1,223,224,223,1102,53,40,225,1001,35,35,224,1001,224,-94,224,4,224,102,8,223,223,101,6,224,224,1,223,224,223,1101,5,73,225,1002,191,52,224,1001,224,-1872,224,4,224,1002,223,8,223,1001,224,5,224,1,223,224,223,102,82,195,224,101,-738,224,224,4,224,1002,223,8,223,1001,224,2,224,1,224,223,223,1101,83,52,225,1101,36,77,225,1101,9,10,225,1,113,187,224,1001,224,-136,224,4,224,1002,223,8,223,101,2,224,224,1,224,223,223,4,223,99,0,0,0,677,0,0,0,0,0,0,0,0,0,0,0,1105,0,99999,1105,227,247,1105,1,99999,1005,227,99999,1005,0,256,1105,1,99999,1106,227,99999,1106,0,265,1105,1,99999,1006,0,99999,1006,227,274,1105,1,99999,1105,1,280,1105,1,99999,1,225,225,225,1101,294,0,0,105,1,0,1105,1,99999,1106,0,300,1105,1,99999,1,225,225,225,1101,314,0,0,106,0,0,1105,1,99999,1007,226,226,224,1002,223,2,223,1006,224,329,1001,223,1,223,1108,226,226,224,102,2,223,223,1006,224,344,101,1,223,223,1007,677,677,224,102,2,223,223,1006,224,359,101,1,223,223,1108,677,226,224,1002,223,2,223,1005,224,374,1001,223,1,223,7,677,226,224,102,2,223,223,1005,224,389,1001,223,1,223,1008,677,677,224,1002,223,2,223,1005,224,404,101,1,223,223,108,226,226,224,1002,223,2,223,1006,224,419,101,1,223,223,1008,226,677,224,1002,223,2,223,1006,224,434,1001,223,1,223,1107,677,226,224,1002,223,2,223,1005,224,449,101,1,223,223,1008,226,226,224,102,2,223,223,1005,224,464,1001,223,1,223,8,226,226,224,1002,223,2,223,1006,224,479,1001,223,1,223,107,226,677,224,102,2,223,223,1005,224,494,1001,223,1,223,7,226,226,224,102,2,223,223,1005,224,509,1001,223,1,223,107,226,226,224,102,2,223,223,1005,224,524,101,1,223,223,107,677,677,224,1002,223,2,223,1006,224,539,101,1,223,223,8,677,226,224,1002,223,2,223,1006,224,554,101,1,223,223,1107,677,677,224,1002,223,2,223,1005,224,569,101,1,223,223,108,226,677,224,1002,223,2,223,1006,224,584,101,1,223,223,7,226,677,224,1002,223,2,223,1005,224,599,1001,223,1,223,8,226,677,224,102,2,223,223,1006,224,614,1001,223,1,223,108,677,677,224,1002,223,2,223,1006,224,629,1001,223,1,223,1007,226,677,224,1002,223,2,223,1006,224,644,101,1,223,223,1108,226,677,224,102,2,223,223,1005,224,659,1001,223,1,223,1107,226,677,224,102,2,223,223,1006,224,674,1001,223,1,223,4,223,99,226};
+
+
 
 
 const auto printInput = [](auto& input) {
@@ -64,15 +66,23 @@ const auto convertToVector = [](int value) {
     };
 
 const auto extractOpCodeAndParams = [](int value) {
+    if (value == 3) return OpCode{OpCodes::STORE, std::vector<int>{0}};
+    if (value == 4) return OpCode{OpCodes::OUTPUT, std::vector<int>{0}};
     const auto v = convertToVector(value);
     OpCode oc;
     auto code = v[v.size() - 2] * 10;
     code += v.back();
-    assert(code == 99 || code == 1 || code == 2 || code == 3 || code == 4);
+    if (!(code == 99 || code == 1 || code == 2 || code == 3 || code == 4)) {
+        std::cout << "ALARM! " << code << " from : " << value << '\n';
+    }
     oc.code = static_cast<OpCodes>(code);
+
     //iterate over every single parameter
+    int j = 0;
+    if (code == 1 || code == 2) oc.param_modes = {0, 0, 0};
+    else if (code == 3 || code ==4 ) oc.param_modes = {0};
     for (int i = v.size() - 3; i >= 0; --i) {
-        oc.param_modes.push_back(v[i]);
+        oc.param_modes[j++] = v[i];
     }
 
     return oc;
@@ -81,46 +91,65 @@ const auto extractOpCodeAndParams = [](int value) {
 const auto parseOpCode = [](auto value) -> OpCode {
     if (static_cast<OpCodes>(value) == OpCodes::STOP)
         return {OpCodes::STOP, {}};
-    else if (static_cast<OpCodes>(value) == OpCodes::STOP)
-        return {OpCodes::STOP, {}};
-    else if (static_cast<OpCodes>(value) == OpCodes::STOP)
-        return {OpCodes::STOP, {}};
-    else extractOpCodeAndParams(value);
+    else return extractOpCodeAndParams(value);
 };
 
-const auto runProgram = [](int noun, int verb) {
+const auto runProgram = [](int param) {
     auto input = input_org;
     //auto opCode = 42;
     int index = 0u;
-    input[1] = noun;
-    input[2] = verb;
-    for (auto i = 0u; i < input.size(); i += 4) {
+    //input[1] = noun;
+    //input[2] = verb;
+    for (auto i = 0u; i < input.size();) {
         index = i;
         auto code = parseOpCode(input[index]);
         auto opCode = code.code;
 
         if (opCode == OpCodes::STOP) {
+            index += 1;
             break;
         }
 
-        const auto firstOperandPos = input[index + 1];
-        const auto secondOperandPos = input[index + 2];
-        const auto resultPos = input[index + 3];
-
         if (opCode == OpCodes::ADD) {
-            const auto res = input[firstOperandPos] + input[secondOperandPos];
-            input[resultPos] = res;
+            const auto firstOperandPos = input[index + 1];
+            const auto secondOperandPos = input[index + 2];
+            const auto resultPos = input[index + 3];
+            index += 4;
+            const auto res = code.param_modes[0] == 1 ? input[firstOperandPos]: firstOperandPos
+            + code.param_modes[1] == 1? input[secondOperandPos] : secondOperandPos;
+            const auto pos = code.param_modes[2] == 1 ? input[resultPos] : resultPos;
+            input[pos] = res;
+            std::cout << "ADD: input[" << pos << "] = " << res << '\n';
         }
         else if (opCode == OpCodes::MULTIPLY) {
-            const auto res = input[firstOperandPos] * input[secondOperandPos];
-            input[resultPos] = res;
+            const auto firstOperandPos = input[index + 1];
+            const auto secondOperandPos = input[index + 2];
+            const auto resultPos = input[index + 3];
+            index += 4;
+            const auto res = code.param_modes[0] == 1 ? input[firstOperandPos]: firstOperandPos
+            * code.param_modes[1] == 1? input[secondOperandPos] : secondOperandPos;
+            //const auto res = input[firstOperandPos] * input[secondOperandPos];
+            const auto pos = code.param_modes[2] == 1 ? input[resultPos] : resultPos;
+            input[pos] = res;
+            std::cout << "MULTIPLY: input[" << pos << "] = " << res << '\n';
+        }
+        else if (opCode == OpCodes::STORE) {
+            const auto firstOperandPos = input[index + 1];
+            const auto pos = code.param_modes[0] == 1 ? input[firstOperandPos] : firstOperandPos;
+            input[pos] = param;
+            index += 2;
+            std::cout << "STORE: input[" << pos << "] = " << param << '\n';
         }
         else if (opCode == OpCodes::OUTPUT) {
+            const auto firstOperandPos = input[index + 1];
+            index += 2;
             std::cout << "OUTPUT: \n";
         }
         else {
-            std::cout << "ZONK - " << opCode << "\n";
+            index += 1;
+            std::cout << "ZONK - " << opCode << " on index: " << index << "\n";
         }
+        i = index;
     }
     return input[0];
 };
@@ -128,20 +157,9 @@ const auto runProgram = [](int noun, int verb) {
 
 int main()
 {
-    auto f = runProgram(12, 2);
+    auto f = runProgram(1);
     auto s = 0;
-    bool stop = false;
-    for (int i = 0; i <= 99; ++i) {
-        if (stop) break;
-        for (int j = 0; j <= 99; ++j) {
-            if (runProgram(i, j) == SECOND_PUZZLE) {
-                s = 100 * i + j;
-                std::cout << i << " - " << j << '\n';
-                stop = true;
-                break;
-            }
-        }
-    }
+
     std::cout << "first: " << f << " second: " << s << '\n';
     return 0;
 }
