95fecd7e055c122c1461e1f3c0071cd753d40e44
Stanislaw Kubiak <extern.stanislaw.kubiak@elektrobit.com>
2019-12-09 10:43:22 +0100

    Day 7 finished

--- day07/main.cpp
+++ day07/main.cpp
@@ -52,7 +52,6 @@ struct OpCode {
 };
 
 auto SECOND_PUZZLE = 19690720;
-//std::vector input_org = { 3,225,1,225,6,6,1100,1,238,225,104,0,1102,59,58,224,1001,224,-3422,224,4,224,102,8,223,223,101,3,224,224,1,224,223,223,1101,59,30,225,1101,53,84,224,101,-137,224,224,4,224,1002,223,8,223,101,3,224,224,1,223,224,223,1102,42,83,225,2,140,88,224,1001,224,-4891,224,4,224,1002,223,8,223,1001,224,5,224,1,223,224,223,1101,61,67,225,101,46,62,224,1001,224,-129,224,4,224,1002,223,8,223,101,5,224,224,1,223,224,223,1102,53,40,225,1001,35,35,224,1001,224,-94,224,4,224,102,8,223,223,101,6,224,224,1,223,224,223,1101,5,73,225,1002,191,52,224,1001,224,-1872,224,4,224,1002,223,8,223,1001,224,5,224,1,223,224,223,102,82,195,224,101,-738,224,224,4,224,1002,223,8,223,1001,224,2,224,1,224,223,223,1101,83,52,225,1101,36,77,225,1101,9,10,225,1,113,187,224,1001,224,-136,224,4,224,1002,223,8,223,101,2,224,224,1,224,223,223,4,223,99,0,0,0,677,0,0,0,0,0,0,0,0,0,0,0,1105,0,99999,1105,227,247,1105,1,99999,1005,227,99999,1005,0,256,1105,1,99999,1106,227,99999,1106,0,265,1105,1,99999,1006,0,99999,1006,227,274,1105,1,99999,1105,1,280,1105,1,99999,1,225,225,225,1101,294,0,0,105,1,0,1105,1,99999,1106,0,300,1105,1,99999,1,225,225,225,1101,314,0,0,106,0,0,1105,1,99999,1007,226,226,224,1002,223,2,223,1006,224,329,1001,223,1,223,1108,226,226,224,102,2,223,223,1006,224,344,101,1,223,223,1007,677,677,224,102,2,223,223,1006,224,359,101,1,223,223,1108,677,226,224,1002,223,2,223,1005,224,374,1001,223,1,223,7,677,226,224,102,2,223,223,1005,224,389,1001,223,1,223,1008,677,677,224,1002,223,2,223,1005,224,404,101,1,223,223,108,226,226,224,1002,223,2,223,1006,224,419,101,1,223,223,1008,226,677,224,1002,223,2,223,1006,224,434,1001,223,1,223,1107,677,226,224,1002,223,2,223,1005,224,449,101,1,223,223,1008,226,226,224,102,2,223,223,1005,224,464,1001,223,1,223,8,226,226,224,1002,223,2,223,1006,224,479,1001,223,1,223,107,226,677,224,102,2,223,223,1005,224,494,1001,223,1,223,7,226,226,224,102,2,223,223,1005,224,509,1001,223,1,223,107,226,226,224,102,2,223,223,1005,224,524,101,1,223,223,107,677,677,224,1002,223,2,223,1006,224,539,101,1,223,223,8,677,226,224,1002,223,2,223,1006,224,554,101,1,223,223,1107,677,677,224,1002,223,2,223,1005,224,569,101,1,223,223,108,226,677,224,1002,223,2,223,1006,224,584,101,1,223,223,7,226,677,224,1002,223,2,223,1005,224,599,1001,223,1,223,8,226,677,224,102,2,223,223,1006,224,614,1001,223,1,223,108,677,677,224,1002,223,2,223,1006,224,629,1001,223,1,223,1007,226,677,224,1002,223,2,223,1006,224,644,101,1,223,223,1108,226,677,224,102,2,223,223,1005,224,659,1001,223,1,223,1107,226,677,224,102,2,223,223,1006,224,674,1001,223,1,223,4,223,99,226};
 std::vector input_org = { 3,8,1001,8,10,8,105,1,0,0,21,38,63,80,105,118,199,280,361,442,99999,3,9,102,5,9,9,1001,9,3,9,1002,9,2,9,4,9,99,3,9,1001,9,4,9,102,4,9,9,101,4,9,9,102,2,9,9,101,2,9,9,4,9,99,3,9,1001,9,5,9,102,4,9,9,1001,9,4,9,4,9,99,3,9,101,3,9,9,1002,9,5,9,101,3,9,9,102,5,9,9,101,3,9,9,4,9,99,3,9,1002,9,2,9,1001,9,4,9,4,9,99,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,1,9,4,9,99,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,99,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,2,9,9,4,9,99,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,1,9,4,9,99,3,9,1001,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,101,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,99 };
 
 
@@ -104,14 +103,15 @@ const auto parseOpCode = [](auto value) -> OpCode {
     else return extractOpCodeAndParams(value);
 };
 
-const auto runProgram = [](int firstParam, int secondParam, std::vector<int>& input) {
+const auto runProgram = [](int paramA, int paramB, int ip, bool& firstParamUsed, std::vector<int>& input) {
     //auto input = input_org;
     auto answer = 0;
-    int index = 0u;
-    bool firstParamUsed = false;
+    int index = ip;
+    OpCodes latestCode;
     for (auto i = 0u; i < input.size();) {
         auto code = parseOpCode(input[index]);
         auto opCode = code.code;
+        latestCode = opCode;
 
         if (opCode == OpCodes::STOP) {
             index += 1;
@@ -122,40 +122,43 @@ const auto runProgram = [](int firstParam, int secondParam, std::vector<int>& in
             const auto firstOperandPos = input[index + 1];
             const auto secondOperandPos = input[index + 2];
             const auto resultPos = input[index + 3];
-            index += 4;
             const auto firstParam = code.param_modes[0] == 0 ? input[firstOperandPos] : firstOperandPos;
             const auto secondParam = code.param_modes[1] == 0 ? input[secondOperandPos] : secondOperandPos;
             const auto res = firstParam + secondParam;
             input[resultPos] = res;
             //std::cout << "ADD: input[" << resultPos << "] = " << res << '\n';
+
+            index += 4;
         }
         else if (opCode == OpCodes::MULTIPLY) {
             const auto firstOperandPos = input[index + 1];
             const auto secondOperandPos = input[index + 2];
             const auto resultPos = input[index + 3];
-            index += 4;
             const auto firstParam = code.param_modes[0] == 0 ? input[firstOperandPos] : firstOperandPos;
             const auto secondParam = code.param_modes[1] == 0 ? input[secondOperandPos] : secondOperandPos;
             const auto res = firstParam * secondParam;
-
             input[resultPos] = res;
             //std::cout << "MULTIPLY: input[" << resultPos << "] = " << res << '\n';
+
+            index += 4;
         }
         else if (opCode == OpCodes::STORE) {
             const auto firstOperandPos = input[index + 1];
             const auto pos = firstOperandPos;
-            input[pos] = !firstParamUsed ? firstParam : secondParam;
-            if (!firstParamUsed) firstParamUsed = true;
-            index += 2;
+            input[pos] = firstParamUsed ? paramB : paramA;
+            firstParamUsed = true;
             //std::cout << "STORE: input[" << pos << "] = " << param << '\n';
+
+            index += 2;
         }
         else if (opCode == OpCodes::OUTPUT) {
             const auto firstParamVal = input[index + 1];
             const auto val = code.param_modes[0] == 0 ? input[firstParamVal] : firstParamVal;
-            index += 2;
             //std::cout << '>' << val << "<\n";
             //return val;
             answer = val;
+            index += 2;
+            break;
         }
         else if (opCode == OpCodes::JUMP_TRUE) {
             const auto firstOperandVal = input[index + 1];
@@ -204,7 +207,6 @@ const auto runProgram = [](int firstParam, int secondParam, std::vector<int>& in
             if (firstParam == secondParam) {
                 input[pos] = 1;
                 //std::cout << "EQUALS: input[" << pos << "] = 1\n";
-
             }
             else {
                 input[pos] = 0;
@@ -218,7 +220,7 @@ const auto runProgram = [](int firstParam, int secondParam, std::vector<int>& in
         }
         i = index;
     }
-    return answer;
+    return std::make_tuple(answer, latestCode, index);
 };
 }
 
@@ -230,32 +232,46 @@ struct Amp {
     }
 
     int process(int inputParam) {
-        return runProgram(phase, inputParam, mInput);
+        const auto [signal, code, instruction_pointer] = runProgram(phase, inputParam, ip, firstParamUsed, mInput);
+        isFinished = (code == OpCodes::STOP);
+        ip = instruction_pointer;
+        return signal;
     }
 
+    bool isRunning() const {
+        return !isFinished;
+    }
+private:
     int phase;
+    int ip {0};
     std::vector<int> mInput;
+    bool isFinished {false};
+    bool firstParamUsed{false};
 };
 
 int main()
 {
-    std::vector<int> thrusters = { 0,1,2,3,4 };
-    auto sum = 0;
-    std::vector<int> answer;
+    std::vector<int> thrusters = { 5,6,7,8,9 };
+    unsigned long long sum = 0u;
     do {
         std::vector<Amp> amps;
         for (auto t : thrusters) {
             amps.push_back({t, input_org});
         }
+        auto outE = 0;
+        while (amps[4].isRunning()) {
+            const auto outA = amps[0].process(outE);
+            const auto outB = amps[1].process(outA);
+            const auto outC = amps[2].process(outB);
+            const auto outD = amps[3].process(outC);
+            outE            = amps[4].process(outD);
 
-        const auto temp_sum = amps[4].process(amps[3].process(amps[2].process(amps[1].process(amps[0].process(0)))));
-        if (temp_sum > sum) {
-            sum = temp_sum;
-            answer = thrusters;
+            if (outE > sum) {
+                sum = outE;
+            }
         }
     } while (std::next_permutation(thrusters.begin(), thrusters.end()));
 
-    for (auto t : answer) std::cout << t;
-    std::cout << " - " << sum << '\n';
+    std::cout << sum << '\n';
     return 0;
 }
