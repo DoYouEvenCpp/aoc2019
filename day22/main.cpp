8ee7fd26ebbbc658ee052b2e12db08f44fa59833
Stanislaw Kubiak <extern.stanislaw.kubiak@elektrobit.com>
2020-01-17 14:37:07 +0100

    Day22 initial commit

---
+++ day22/main.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+#include <set>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <map>
+#include <array>
+#include <cmath>
+#include <tuple>
+#include <cassert>
+#include <optional>
+#include <functional>
+#include <stack>
+#include <cassert>
+#include <queue>
+#include <list>
+#include <fstream>
+#include <thread>
+#include <chrono>
+#include <unordered_set>
+
+
+namespace{
+
+}
+
+int main() {
+
+}
