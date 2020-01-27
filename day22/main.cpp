d1db4c4fdb4f491a1461d6882177f84fe701f1b5
Stanislaw Kubiak <extern.stanislaw.kubiak@elektrobit.com>
2020-01-27 09:18:23 +0100

    Initial implementation for day22

--- day22/main.cpp
+++ day22/main.cpp
@@ -18,12 +18,48 @@
 #include <thread>
 #include <chrono>
 #include <unordered_set>
+#include <unordered_map>
 
 
 namespace{
+constexpr auto new_stack=  "deal into new stack";
+constexpr auto cut = "cut";
+constexpr auto increment = "deal with increment";
 
+enum class task {
+    stack,
+    cut,
+    inc
+};
+
+using data_type = std::vector<std::pair<task, int>>;
+
+const auto loadData = [](auto path){
+    data_type res;
+    std::fstream fs(path, std::fstream::in);
+    std::string line;
+    while (std::getline(fs, line)) {
+        if (auto pos = line.find(new_stack); pos != std::string::npos) {
+            res.push_back(std::make_pair(task::stack, 0));
+        }
+        else if (auto pos = line.find(cut); pos != std::string::npos) {
+            res.push_back(std::make_pair(task::cut, std::stoi(line.substr(pos + 4))));
+        }
+        else if (auto pos = line.find(increment); pos != std::string::npos) {
+            res.push_back(std::make_pair(task::inc, std::stoi(line.substr(pos + 20))));
+        }
+    }
+    fs.close();
+    return res;
+};
 }
 
-int main() {
+int main(int argc, char** argv) {
+    if (argc < 2){
+        std::cout << "too few parameters\n";
+        return -1;
+    }
 
+    const std::string path = argv[1];
+    const auto data = loadData(path);
 }
